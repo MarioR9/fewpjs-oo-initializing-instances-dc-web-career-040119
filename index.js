f6903f81144f26f7f8500b3dcd9c7b42f5278cad
Marior9 <mariorodriguezan@gmail.com>
2019-05-20 17:15:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+
+console.log('connected')
 // Write your code here
+// import { throws } from "assert";
+class Breakfast{
+    constructor(food, drink){
+        this.food = food 
+        this.drink = drink
+    }
+}
+class Lunch{
+    constructor(salad, soup, drink){
+        this.salad = salad 
+        this.soup = soup
+        this.drink = drink
+    }
+}
+class Dinner{
+    constructor(salad, soup, entree, _dessert){
+        this.salad = salad
+        this.soup = soup 
+        this.entree = entree 
+        this._dessert = _dessert
+    }
+}
